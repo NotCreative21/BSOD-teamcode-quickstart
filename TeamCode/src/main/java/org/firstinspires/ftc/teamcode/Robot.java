359dc05e53f77d0d21780b4047d7a36d8081ffb5
1198159 <jeffastedman@gmail.com>
2022-01-28 14:12:13 -0800

    servo ducc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.Range;
 import com.technototes.library.hardware.motor.Motor;
@@ -11,21 +12,18 @@ import com.technototes.library.util.Color;
 
 @Config
 public class Robot implements Loggable {
-    public static double SERVO_OPEN = 1, SERVO_CLOSE=0;
     public static double ARM_UP_SPEED = 0.5, ARM_DOWN_SPEED = -0.5;
     public static double CAROUSEL_LEFT_SPEED, CAROUSEL_RIGHT_SPEED;
 
-    public Motor<DcMotorEx> lDrive1, lDrive2, rDrive1, rDrive2, arm, carousel;
-    public Servo cup;
+    public Motor<DcMotorEx> lDrive1, lDrive2, rDrive1, rDrive2, arm;
+    public Motor<CRServo> carousel;
     public Robot(){
         lDrive1 = new Motor<DcMotorEx>("ldrive1");
         lDrive2 = new Motor<DcMotorEx>("ldrive2");
         rDrive1 = new Motor<DcMotorEx>("rdrive1").invert();
         rDrive2 = new Motor<DcMotorEx>("rdrive2").invert();
         arm = new Motor<DcMotorEx>("arm").brake();
-        carousel = new Motor<DcMotorEx>("spin").brake();
-
-        cup = new Servo("cup").startAt(SERVO_CLOSE);
+        carousel = new Motor<CRServo>("spin").brake();
     }
 
     public void arcadeDrive(double x, double y){
@@ -62,19 +60,7 @@ public class Robot implements Loggable {
     public void carouselStop(){
         carousel.setSpeed(0);
     }
-
-    public void open(){
-        cup.setPosition(SERVO_OPEN);
-    }
-
-    public void close(){
-        cup.setPosition(SERVO_CLOSE);
-    }
-
-    @Log.Boolean(name="Bucket", index=0,trueValue = "OPEN", trueColor = Color.GREEN, falseColor = Color.RED, falseValue = "CLOSED")
-    public boolean getBucket(){
-        return cup.getPosition() == SERVO_OPEN;
-    }
+    
     @Log.NumberSlider(name="Carousel", index=1, slider = Color.YELLOW, sliderBackground = Color.ORANGE)
     public double getCarousel(){
         return carousel.getSpeed();
